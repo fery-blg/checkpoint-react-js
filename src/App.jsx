f5cc2055f288fe4cad40287b04390863002a64e6
fery-blg <ferielblg5@gmail.com>
2024-01-01 21:20:24 +0100

    chekpoint react-js

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from'./Menu';
+import Block from './Block';
+import Heading from './Heading';
+
+function App() {
+
+
+  return (
+    <>
+      <div className='App'>
+        <Menu/>
+        <Heading/>
+        <Block/>
+    
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Heading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+
+function Heading() {
+  return (
+    <div>
+      <h1>
+        Checkpoint <Badge bg="secondary">React-Js</Badge>
+      </h1>
+      
+    </div>
+  );
+}
+
+export default Heading;
